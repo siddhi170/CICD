e1c66a96dd7c14bce46980e3c8fb5aa76dc8dca1
siddhi170 <siddhishinde17904@gmail.com>
2025-02-23 14:56:37 +0530

    Updated message for Jenkins test

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
-
+s
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, Jenkins CI/CD Pipeline!"
+    return "Hello, Jenkins CI/CD Pipeline - Updated!"
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
